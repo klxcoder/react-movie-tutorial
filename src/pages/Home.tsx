d5fb197491a4aa9db13449fa531bb8bab43560a0
klxcoder <klxcoder@gmail.com>
2025-03-09 22:28:34 +0700

    getPopularMovies

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,37 +1,61 @@
-import { FormEvent, useState } from 'react'
+import { FormEvent, useEffect, useState } from 'react'
 import MovieCard from '../components/MovieCard'
 import '../css/Home.css'
+import { getPopularMovies } from '../services/api'
+
+type Movie = {
+  id: number,
+  title: string,
+  url: string,
+  release_date: string,
+}
 
 function Home() {
 
-  const [searchQuery, setSearchQuery] = useState("");
+  const [searchQuery, setSearchQuery] = useState("")
+  const [movies, setMovies] = useState<Movie[]>([])
+  const [error, setError] = useState('')
+  const [loading, setLoading] = useState(true)
 
-  const movies: ({
-    id: number,
-    title: string,
-    url: string,
-    release_date: string,
-  })[] = [
-      {
-        id: 1,
-        title: "John Wick",
-        url: '',
-        release_date: "2020",
-      },
-      {
-        id: 2,
-        title: "Terminator",
-        url: '',
+  useEffect(() => {
+    const loadPopularMovies = async () => {
+      try {
+        const popularMovies = await getPopularMovies();
+        if (!popularMovies) {
+          const movies: Movie[] = [
+            {
+              id: 1,
+              title: "John Wick",
+              url: '',
+              release_date: "2020",
+            },
+            {
+              id: 2,
+              title: "Terminator",
+              url: '',
 
-        release_date: "1999",
-      },
-      {
-        id: 3,
-        title: "The Matrix",
-        url: '',
-        release_date: "1998",
-      },
-    ]
+              release_date: "1999",
+            },
+            {
+              id: 3,
+              title: "The Matrix",
+              url: '',
+              release_date: "1998",
+            },
+          ]
+          setMovies(movies);
+          throw Error('FetchMoviesError: Can not fetch Movies')
+        }
+        setMovies(popularMovies);
+      } catch (err) {
+        console.log(err);
+        setError("Failed to load movies...")
+      } finally {
+        setLoading(false);
+      }
+    }
+    loadPopularMovies()
+  }, [])
 
   const handleSearch = (e: FormEvent) => {
     e.preventDefault();
@@ -58,14 +82,21 @@ function Home() {
           Search
         </button>
       </form>
-      <div className="movies-grid">
-        {movies.map(movie => (
-          <MovieCard
-            key={movie.id}
-            movie={movie}
-          />
-        ))}
-      </div>
+      {error && <div className="error-message">{error}</div>}
+      {loading ? (
+        <div className="loading">
+          Loading...
+        </div>
+      ) : (
+        <div className="movies-grid">
+          {movies.map(movie => (
+            <MovieCard
+              key={movie.id}
+              movie={movie}
+            />
+          ))}
+        </div>
+      )}
     </div>
   )
 }
