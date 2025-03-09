a9d8c2bdcaba0bb934eec658abdc59d60133d4c3
klxcoder <klxcoder@gmail.com>
2025-03-09 20:30:14 +0700

    MovieCard

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import './App.css'
+import MovieCard from './components/MovieCard'
 
 function App() {
 
   return (
     <div>
-      Minimal
+      <MovieCard
+        movie={{
+          title: "Film",
+          url: "",
+          release_date: "2024",
+        }}
+      />
     </div>
   )
 }
