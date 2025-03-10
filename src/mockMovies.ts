e8f8f3a0e995608dd75f9628add2b53a336a5da7
klxcoder <klxcoder@gmail.com>
2025-03-10 09:44:58 +0700

    301

--- src/mockMovies.ts
+++ src/mockMovies.ts
@@ -3,271 +3,271 @@ import { Movie } from "./components/MovieCard";
 export const mockMovies: Movie[] = [
   {
     "id": "696506-mickey-17",
-    "url": "https://media.themoviedb.org/t/p/w220_and_h330_face/lrCcovGRcuv8Z1v3ae1ZH5Ird05.jpg",
+    "url": "https://image.tmdb.org/t/p/w220_and_h330_face/lrCcovGRcuv8Z1v3ae1ZH5Ird05.jpg",
     "title": "Mickey 17",
     "release_date": "Feb 28, 2025"
   },
   {
     "id": "127532",
-    "url": "https://media.themoviedb.org/t/p/w220_and_h330_face/geCRueV3ElhRTr0xtJuEWJt6dJ1.jpg",
+    "url": "https://image.tmdb.org/t/p/w220_and_h330_face/geCRueV3ElhRTr0xtJuEWJt6dJ1.jpg",
     "title": "Solo Leveling",
     "release_date": "Jan 07, 2024"
   },
   {
     "id": "1104845-plankton-the-movie",
-    "url": "https://media.themoviedb.org/t/p/w220_and_h330_face/iqQw58Cbgh5r7XRYUZpJPDPQD0m.jpg",
+    "url": "https://image.tmdb.org/t/p/w220_and_h330_face/iqQw58Cbgh5r7XRYUZpJPDPQD0m.jpg",
     "title": "Plankton: The Movie",
     "release_date": "Mar 07, 2025"
   },
   {
     "id": "822119-captain-america-brave-new-world",
-    "url": "https://media.themoviedb.org/t/p/w220_and_h330_face/pzIddUEMWhWzfvLI3TwxUG2wGoi.jpg",
+    "url": "https://image.tmdb.org/t/p/w220_and_h330_face/pzIddUEMWhWzfvLI3TwxUG2wGoi.jpg",
     "title": "Captain America: Brave New World",
     "release_date": "Feb 14, 2025"
   },
   {
     "id": "1064213-anora",
-    "url": "https://media.themoviedb.org/t/p/w220_and_h330_face/qh8m8Udz0sCa5gy9VaqfHPh0yPM.jpg",
+    "url": "https://image.tmdb.org/t/p/w220_and_h330_face/qh8m8Udz0sCa5gy9VaqfHPh0yPM.jpg",
     "title": "Anora",
     "release_date": "Oct 14, 2024"
   },
   {
     "id": "823219-straume",
-    "url": "https://media.themoviedb.org/t/p/w220_and_h330_face/imKSymKBK7o73sajciEmndJoVkR.jpg",
+    "url": "https://image.tmdb.org/t/p/w220_and_h330_face/imKSymKBK7o73sajciEmndJoVkR.jpg",
     "title": "Flow",
     "release_date": "Mar 07, 2025"
   },
   {
     "id": "95396-severance",
-    "url": "https://media.themoviedb.org/t/p/w220_and_h330_face/pPHpeI2X1qEd1CS1SeyrdhZ4qnT.jpg",
+    "url": "https://image.tmdb.org/t/p/w220_and_h330_face/pPHpeI2X1qEd1CS1SeyrdhZ4qnT.jpg",
     "title": "Severance",
     "release_date": "Feb 17, 2022"
   },
   {
     "id": "202555-daredevil-born-again",
-    "url": "https://media.themoviedb.org/t/p/w220_and_h330_face/9lLuhV703HGCbnz6FxnqCwIwzAZ.jpg",
+    "url": "https://image.tmdb.org/t/p/w220_and_h330_face/9lLuhV703HGCbnz6FxnqCwIwzAZ.jpg",
     "title": "Daredevil: Born Again",
     "release_date": "Mar 04, 2025"
   },
   {
     "id": "950396-the-gorge",
-    "url": "https://media.themoviedb.org/t/p/w220_and_h330_face/7iMBZzVZtG0oBug4TfqDb9ZxAOa.jpg",
+    "url": "https://image.tmdb.org/t/p/w220_and_h330_face/7iMBZzVZtG0oBug4TfqDb9ZxAOa.jpg",
     "title": "The Gorge",
     "release_date": "Feb 14, 2025"
   },
   {
     "id": "1084199-companion",
-    "url": "https://media.themoviedb.org/t/p/w220_and_h330_face/oCoTgC3UyWGfyQ9thE10ulWR7bn.jpg",
+    "url": "https://image.tmdb.org/t/p/w220_and_h330_face/oCoTgC3UyWGfyQ9thE10ulWR7bn.jpg",
     "title": "Companion",
     "release_date": "Feb 14, 2025"
   },
   {
     "id": "402431-wicked",
-    "url": "https://media.themoviedb.org/t/p/w220_and_h330_face/xDGbZ0JJ3mYaGKy4Nzd9Kph6M9L.jpg",
+    "url": "https://image.tmdb.org/t/p/w220_and_h330_face/xDGbZ0JJ3mYaGKy4Nzd9Kph6M9L.jpg",
     "title": "Wicked",
     "release_date": "Nov 29, 2024"
   },
   {
     "id": "108978-reacher",
-    "url": "https://media.themoviedb.org/t/p/w220_and_h330_face/31GlRQMiDunO8cl3NxTz34U64rf.jpg",
+    "url": "https://image.tmdb.org/t/p/w220_and_h330_face/31GlRQMiDunO8cl3NxTz34U64rf.jpg",
     "title": "Reacher",
     "release_date": "Feb 03, 2022"
   },
   {
     "id": "1204892-picture-this",
-    "url": "https://media.themoviedb.org/t/p/w220_and_h330_face/r8mUV5bJlUQrco0XWYHUiSVzgdh.jpg",
+    "url": "https://image.tmdb.org/t/p/w220_and_h330_face/r8mUV5bJlUQrco0XWYHUiSVzgdh.jpg",
     "title": "Picture This",
     "release_date": "Mar 05, 2025"
   },
   {
     "id": "128904-dark-winds",
-    "url": "https://media.themoviedb.org/t/p/w220_and_h330_face/kDlrMN0P9ByrEH2GoRbBFeZHbcf.jpg",
+    "url": "https://image.tmdb.org/t/p/w220_and_h330_face/kDlrMN0P9ByrEH2GoRbBFeZHbcf.jpg",
     "title": "Dark Winds",
     "release_date": "Jun 12, 2022"
   },
   {
     "id": "157744-1923",
-    "url": "https://media.themoviedb.org/t/p/w220_and_h330_face/97YimtKZCD0bvHIG4YPgWo3OYar.jpg",
+    "url": "https://image.tmdb.org/t/p/w220_and_h330_face/97YimtKZCD0bvHIG4YPgWo3OYar.jpg",
     "title": "1923",
     "release_date": "Dec 18, 2022"
   },
   {
     "id": "974576-conclave",
-    "url": "https://media.themoviedb.org/t/p/w220_and_h330_face/m5x8D0bZ3eKqIVWZ5y7TnZ2oTVg.jpg",
+    "url": "https://image.tmdb.org/t/p/w220_and_h330_face/m5x8D0bZ3eKqIVWZ5y7TnZ2oTVg.jpg",
     "title": "Conclave",
     "release_date": "Oct 25, 2024"
   },
   {
     "id": "255298",
-    "url": "https://media.themoviedb.org/t/p/w220_and_h330_face/40SJ9NvLBJRCiVM0bpcznI8MJq6.jpg",
+    "url": "https://image.tmdb.org/t/p/w220_and_h330_face/40SJ9NvLBJRCiVM0bpcznI8MJq6.jpg",
     "title": "The Art of Negotiation",
     "release_date": "Mar 08, 2025"
   },
   {
     "id": "940139-here",
-    "url": "https://media.themoviedb.org/t/p/w220_and_h330_face/79euHUJJtfgeGU63ef38KtNjXEn.jpg",
+    "url": "https://image.tmdb.org/t/p/w220_and_h330_face/79euHUJJtfgeGU63ef38KtNjXEn.jpg",
     "title": "Here",
     "release_date": "Oct 30, 2024"
   },
   {
     "id": "933260-the-substance",
-    "url": "https://media.themoviedb.org/t/p/w220_and_h330_face/cGm2qnmXx9tFabmzEIkJZjCJdQd.jpg",
+    "url": "https://image.tmdb.org/t/p/w220_and_h330_face/cGm2qnmXx9tFabmzEIkJZjCJdQd.jpg",
     "title": "The Substance",
     "release_date": "Nov 01, 2024"
   },
   {
     "id": "1418522-delicious",
-    "url": "https://media.themoviedb.org/t/p/w220_and_h330_face/3W1PZYEiDXWLLjLCMUbRPfjHxmq.jpg",
+    "url": "https://image.tmdb.org/t/p/w220_and_h330_face/3W1PZYEiDXWLLjLCMUbRPfjHxmq.jpg",
     "title": "Delicious",
     "release_date": "Mar 07, 2025"
   },
   {
     "id": "549509-the-brutalist",
-    "url": "https://media.themoviedb.org/t/p/w220_and_h330_face/gl16UFazRs5Xv3VS8YKhRqQRY8T.jpg",
+    "url": "https://image.tmdb.org/t/p/w220_and_h330_face/gl16UFazRs5Xv3VS8YKhRqQRY8T.jpg",
     "title": "The Brutalist",
     "release_date": "Dec 20, 2024"
   },
   {
     "id": "1302916-heart-eyes",
-    "url": "https://media.themoviedb.org/t/p/w220_and_h330_face/ebqttF1Vjgde6uSFBEw9mta7Hd3.jpg",
+    "url": "https://image.tmdb.org/t/p/w220_and_h330_face/ebqttF1Vjgde6uSFBEw9mta7Hd3.jpg",
     "title": "Heart Eyes",
     "release_date": "Feb 06, 2025"
   },
   {
     "id": "245927-untitled-dan-fogelman-hulu-series",
-    "url": "https://media.themoviedb.org/t/p/w220_and_h330_face/sJUti0JDtE1IYl7RVJARAGyKHqz.jpg",
+    "url": "https://image.tmdb.org/t/p/w220_and_h330_face/sJUti0JDtE1IYl7RVJARAGyKHqz.jpg",
     "title": "Paradise",
     "release_date": "Jan 26, 2025"
   },
   {
     "id": "661539-a-complete-unknown",
-    "url": "https://media.themoviedb.org/t/p/w220_and_h330_face/llWl3GtNoXosbvYboelmoT459NM.jpg",
+    "url": "https://image.tmdb.org/t/p/w220_and_h330_face/llWl3GtNoXosbvYboelmoT459NM.jpg",
     "title": "A Complete Unknown",
     "release_date": "Dec 18, 2024"
   },
   {
     "id": "95557-invincible",
-    "url": "https://media.themoviedb.org/t/p/w220_and_h330_face/jBn4LWlgdsf6xIUYhYBwpctBVsj.jpg",
+    "url": "https://image.tmdb.org/t/p/w220_and_h330_face/jBn4LWlgdsf6xIUYhYBwpctBVsj.jpg",
     "title": "INVINCIBLE",
     "release_date": "Mar 25, 2021"
   },
   {
     "id": "137228-la-casa-de-los-famosos",
-    "url": "https://media.themoviedb.org/t/p/w220_and_h330_face/mUJMbinr9Ng5FjIRprioaG9VxRU.jpg",
+    "url": "https://image.tmdb.org/t/p/w220_and_h330_face/mUJMbinr9Ng5FjIRprioaG9VxRU.jpg",
     "title": "La Casa de los Famosos",
     "release_date": "Aug 24, 2021"
   },
   {
     "id": "33238",
-    "url": "https://media.themoviedb.org/t/p/w220_and_h330_face/2Wmmu1MkqxJ48J7aySET9EKEjXz.jpg",
+    "url": "https://image.tmdb.org/t/p/w220_and_h330_face/2Wmmu1MkqxJ48J7aySET9EKEjXz.jpg",
     "title": "Running Man",
     "release_date": "Jul 11, 2010"
   },
   {
     "id": "257048-nova-novela-das-seis",
-    "url": "https://media.themoviedb.org/t/p/w220_and_h330_face/jFSkjQSZ5Td52igalpoTQRuHtk.jpg",
+    "url": "https://image.tmdb.org/t/p/w220_and_h330_face/jFSkjQSZ5Td52igalpoTQRuHtk.jpg",
     "title": "She's the One",
     "release_date": "Nov 04, 2024"
   },
   {
     "id": "2224-the-daily-show",
-    "url": "https://media.themoviedb.org/t/p/w220_and_h330_face/ixcfyK7it6FjRM36Te4OdblAq4X.jpg",
+    "url": "https://image.tmdb.org/t/p/w220_and_h330_face/ixcfyK7it6FjRM36Te4OdblAq4X.jpg",
     "title": "The Daily Show",
     "release_date": "Jul 22, 1996"
   },
   {
     "id": "36361-ulice",
-    "url": "https://media.themoviedb.org/t/p/w220_and_h330_face/gFEHva8Csx18hMGJJZ6gi4sFSKR.jpg",
+    "url": "https://image.tmdb.org/t/p/w220_and_h330_face/gFEHva8Csx18hMGJJZ6gi4sFSKR.jpg",
     "title": "Ulice",
     "release_date": "Sep 05, 2005"
   },
   {
     "id": "81329-un-si-grand-soleil",
-    "url": "https://media.themoviedb.org/t/p/w220_and_h330_face/t6jVlbPMtZOJoAOfeoR4yQmnjXM.jpg",
+    "url": "https://image.tmdb.org/t/p/w220_and_h330_face/t6jVlbPMtZOJoAOfeoR4yQmnjXM.jpg",
     "title": "Chronicles of the Sun",
     "release_date": "Aug 27, 2018"
   },
   {
     "id": "250060",
-    "url": "https://media.themoviedb.org/t/p/w220_and_h330_face/iqFr4kgIdJU73YoJr4Vdf7gFZqr.jpg",
+    "url": "https://image.tmdb.org/t/p/w220_and_h330_face/iqFr4kgIdJU73YoJr4Vdf7gFZqr.jpg",
     "title": "The First Frost",
     "release_date": "Feb 18, 2025"
   },
   {
     "id": "30801-1-2",
-    "url": "https://media.themoviedb.org/t/p/w220_and_h330_face/qezeCEkIKHQM5iwyVeYBuXmYA2h.jpg",
+    "url": "https://image.tmdb.org/t/p/w220_and_h330_face/qezeCEkIKHQM5iwyVeYBuXmYA2h.jpg",
     "title": "2 Days and 1 Night",
     "release_date": "Aug 05, 2007"
   },
   {
     "id": "72879-demain-nous-appartient",
-    "url": "https://media.themoviedb.org/t/p/w220_and_h330_face/zMWldNZF0wS3L5XkDVFHxYhclcL.jpg",
+    "url": "https://image.tmdb.org/t/p/w220_and_h330_face/zMWldNZF0wS3L5XkDVFHxYhclcL.jpg",
     "title": "Tomorrow Is Ours",
     "release_date": "Jul 17, 2017"
   },
   {
     "id": "22980-watch-what-happens-live-with-andy-cohen",
-    "url": "https://media.themoviedb.org/t/p/w220_and_h330_face/onSD9UXfJwrMXWhq7UY7hGF2S1h.jpg",
+    "url": "https://image.tmdb.org/t/p/w220_and_h330_face/onSD9UXfJwrMXWhq7UY7hGF2S1h.jpg",
     "title": "Watch What Happens Live with Andy Cohen",
     "release_date": "Jul 16, 2009"
   },
   {
     "id": "63770-the-late-show-with-stephen-colbert",
-    "url": "https://media.themoviedb.org/t/p/w220_and_h330_face/9jkThAGYj2yp8jsS6Nriy5mzKFT.jpg",
+    "url": "https://image.tmdb.org/t/p/w220_and_h330_face/9jkThAGYj2yp8jsS6Nriy5mzKFT.jpg",
     "title": "The Late Show with Stephen Colbert",
     "release_date": "Sep 08, 2015"
   },
   {
     "id": "70672",
-    "url": "https://media.themoviedb.org/t/p/w220_and_h330_face/2jIi55JtYKJTL1km8qHMuUilOWo.jpg",
+    "url": "https://image.tmdb.org/t/p/w220_and_h330_face/2jIi55JtYKJTL1km8qHMuUilOWo.jpg",
     "title": "Men on a Mission",
     "release_date": "Dec 05, 2015"
   },
   {
     "id": "13008-tmz-on-tv",
-    "url": "https://media.themoviedb.org/t/p/w220_and_h330_face/ifFpUG51DJaQxwnZ35VwoGRFGGv.jpg",
+    "url": "https://image.tmdb.org/t/p/w220_and_h330_face/ifFpUG51DJaQxwnZ35VwoGRFGGv.jpg",
     "title": "TMZ",
     "release_date": "Sep 10, 2007"
   },
   {
     "id": "206559-binnelanders",
-    "url": "https://media.themoviedb.org/t/p/w220_and_h330_face/3bzECfllho8PphdYujLUIuhncJD.jpg",
+    "url": "https://image.tmdb.org/t/p/w220_and_h330_face/3bzECfllho8PphdYujLUIuhncJD.jpg",
     "title": "Binnelanders",
     "release_date": "Oct 13, 2005"
   },
   {
     "id": "235484-suidooster",
-    "url": "https://media.themoviedb.org/t/p/w220_and_h330_face/naCgSiacvV685kait6fBvhVhdce.jpg",
+    "url": "https://image.tmdb.org/t/p/w220_and_h330_face/naCgSiacvV685kait6fBvhVhdce.jpg",
     "title": "Suidooster",
     "release_date": "Nov 16, 2015"
   },
   {
     "id": "280749-secret-story-desafio-final",
-    "url": "https://media.themoviedb.org/t/p/w220_and_h330_face/vLUonwbZ2im6iitunAxlDIGCfdZ.jpg",
+    "url": "https://image.tmdb.org/t/p/w220_and_h330_face/vLUonwbZ2im6iitunAxlDIGCfdZ.jpg",
     "title": "Secret Story - Desafio Final",
     "release_date": "Jan 06, 2013"
   },
   {
     "id": "237478-mania-de-voce",
-    "url": "https://media.themoviedb.org/t/p/w220_and_h330_face/xq1X6H8pupwx0jeZpu4Piq0V5uL.jpg",
+    "url": "https://image.tmdb.org/t/p/w220_and_h330_face/xq1X6H8pupwx0jeZpu4Piq0V5uL.jpg",
     "title": "Crazy About You",
     "release_date": "Sep 09, 2024"
   },
   {
     "id": "4496-meet-the-press",
-    "url": "https://media.themoviedb.org/t/p/w220_and_h330_face/lisjDmT2xTykSZxCNvd7E3gQ9AI.jpg",
+    "url": "https://image.tmdb.org/t/p/w220_and_h330_face/lisjDmT2xTykSZxCNvd7E3gQ9AI.jpg",
     "title": "Meet the Press",
     "release_date": "Nov 06, 1947"
   },
   {
     "id": "257064-volta-por-cima",
-    "url": "https://media.themoviedb.org/t/p/w220_and_h330_face/nyN8R0P1Hqwq7ksJz4O2BIAUd4W.jpg",
+    "url": "https://image.tmdb.org/t/p/w220_and_h330_face/nyN8R0P1Hqwq7ksJz4O2BIAUd4W.jpg",
     "title": "Volta por Cima",
     "release_date": "Sep 30, 2024"
   },
   {
     "id": "240909-la-casa-de-los-famosos-colombia",
-    "url": "https://media.themoviedb.org/t/p/w220_and_h330_face/xCvZ0H1RiWhU6yFtzRJL3PSI2jF.jpg",
+    "url": "https://image.tmdb.org/t/p/w220_and_h330_face/xCvZ0H1RiWhU6yFtzRJL3PSI2jF.jpg",
     "title": "La Casa de los Famosos Colombia",
     "release_date": "Feb 11, 2024"
   }
