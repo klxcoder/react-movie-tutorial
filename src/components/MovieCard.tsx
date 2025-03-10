9f8d4b2afd80206078c362d8b09e2012882227ba
klxcoder <klxcoder@gmail.com>
2025-03-10 10:03:58 +0700

    BASE_URL

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -29,7 +29,7 @@ function MovieCard({
 
   return (<div className="movie-card">
     <div className="movie-poster">
-      <img src={movie.url} alt={movie.title} />
+      <img src={`${import.meta.env.BASE_URL}${movie.url}`} alt={movie.title} />
       <div className="movie-overlay">
         <button
           className={`favorite-btn ${favorite ? "active" : ""}`}
